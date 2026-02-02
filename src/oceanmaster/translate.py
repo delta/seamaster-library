bdddb144bc0009bb5e3113b6eb93014dd38d2d17
niharika896 <niharika9agarwal@gmail.com>
2026-02-02 19:06:03 +0530

    fix:merged ActionType and Abilities

--- src/oceanmaster/translate.py
+++ src/oceanmaster/translate.py
@@ -4,7 +4,7 @@ Provides functions to create various game actions.
 
 from oceanmaster.models.point import Point
 from oceanmaster.models.action import Action
-from oceanmaster.constants import ActionType, Direction
+from oceanmaster.constants import Direction,Ability
 from oceanmaster.utils import BotIDAllocator
 
 BOT_ID_ALLOCATOR = BotIDAllocator()
@@ -14,47 +14,47 @@ def move(direction: Direction):
     """
     Creates a move action in the specified direction.
     """
-    return Action(ActionType.MOVE, {"direction": direction.value})
+    return Action(Ability.MOVE, {"direction": direction.value})
 
 
 def move_speed(direction: Direction, step: int):
     """
     Creates a move action in the specified direction with a given step size.
     """
-    return Action(ActionType.MOVE, {"direction": direction.value, "step": step})
+    return Action(Ability.MOVE, {"direction": direction.value, "step": step})
 
 
 def harvest(direction: Direction):
     """ "
     Creates a harvest action in the specified direction.
     """
-    return Action(ActionType.HARVEST, {"direction": direction.value})
+    return Action(Ability.HARVEST, {"direction": direction.value})
 
 
 def self_destruct():
     """
     Creates a self-destruct action.
     """
-    return Action(ActionType.SELF_DESTRUCT, {"direction": "NULL"})
+    return Action(Ability.SELF_DESTRUCT, {"direction": "NULL"})
 
 
 def defend():
     """
     Creates a defend action.
     """
-    return Action(ActionType.DEFEND, {"direction": "NULL"})
+    return Action(Ability.DEFEND, {"direction": "NULL"})
 
 
-def spawn(abilities: list[str], location: int):
+def spawn(Ability: list[str], location: int):
     """
-    Creates a spawn action with specified abilities at a given location.
+    Creates a spawn action with specified Ability at a given location.
     """
     bot_id = BOT_ID_ALLOCATOR.allocate()
-    return bot_id, {"abilities": abilities, "location": {"x": location, "y": 0}}
+    return bot_id, {"Ability": Ability, "location": {"x": location, "y": 0}}
 
 
 def lockpick(location: Point):
     """
     Creates a lockpick action.
     """
-    return Action(ActionType.LOCKPICK, {"location": location})
+    return Action(Ability.LOCKPICK, {"location": location})
